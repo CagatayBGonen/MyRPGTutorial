b88d9b8cef92db18dca92266d9a68f7bdc1dc72e
Cagatay Bayram Gonen <cagataybayramgonen@gmail.com>
2022-11-12 10:42:23 +0300

    Revert "Merge pull request #2 from CagatayBGonen/CagatayDev"
    This reverts commit b781e53cf671c355539fafdf2302d41d2099c881, reversing
    changes made to 252b9f5d0deb850b6ff8c25ac5aa6012bffb5e53.

--- Assets/Scripts/PlayerMotor.cs
+++ Assets/Scripts/PlayerMotor.cs
@@ -6,9 +6,6 @@ using UnityEngine.AI;
 [RequireComponent(typeof(NavMeshAgent))]
 public class PlayerMotor : MonoBehaviour
 {
-    //Variable for current target transform
-    Transform target;
-    //reference to mesh where ai follows for pathfinding
     NavMeshAgent agent;
     // Start is called before the first frame update
     void Start()
@@ -16,47 +13,10 @@ public class PlayerMotor : MonoBehaviour
         //initiliazing agent NavMeshAgent
         agent = GetComponent<NavMeshAgent>();
     }
-    void Update()
-    {
-        if (target != null)
-        {
-            agent.SetDestination(target.position);
-            FaceTarget();
-        }
-    }
 
     // move to player to the point
     public void MoveToPoint(Vector3 point)
     {
         agent.SetDestination(point);
     }
-    //tracking target
-    public void FollowTarget(Interactable newTarget)
-    {
-        //setting the stop position for the player
-        agent.stoppingDistance = newTarget.radius * 0.8f;
-        //to manuelly control agents rotation
-        agent.updateRotation = false;
-        //initialize target transform
-        target = newTarget.interactionTransform;
-    }
-
-    //stop tracking the target
-    public void StopFollowingTarget()
-    {
-        agent.stoppingDistance = 0f;
-        agent.updateRotation = true;
-        target = null;
-    }
-    //agent rotation function
-    void FaceTarget()
-    {
-        //initializing the direction of the target position acc to player position
-        Vector3 direction = (target.position - transform.position).normalized;
-        //how we should rotate to face the target.
-        Quaternion lookRotation = Quaternion.LookRotation(new Vector3(direction.x, 0f, direction.z));
-        //smoothly rotate player
-        transform.rotation = Quaternion.Slerp(transform.rotation, lookRotation, Time.deltaTime * 5f);
-    }
-
 }
